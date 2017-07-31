b91373575fb0b2710c89e2338cf6ab8cc63d4492
Gavin Stark <gavin@gstark.com>
2017-07-31 10:32:08 -0400

    Static home page with todo items

--- server.js
+++ server.js
@@ -1,11 +1,21 @@
 const express = require('express')
+const mustacheExpress = require('mustache-express')
 
 const app = express()
+
+// Configure mustache to be present in our app
+app.engine('mustache', mustacheExpress())
+app.set('views', './templates')
+app.set('view engine', 'mustache')
 
 // When the user asks for `/`, say "Hello world"
 app.get('/', (req, res) => {
   console.log(`${req.connection.remoteAddress} connected to me and asked for /`)
-  res.send('Hello world')
+
+  // Sends a *STRING* to the user
+  // res.send('Hello world')
+
+  res.render('homepage')
 })
 
 app.listen(3000, () => {
