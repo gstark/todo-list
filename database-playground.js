fb9fc31fac8b549e2e1efcfefb35d7cccf91c635
Gavin Stark <gavin@gstark.com>
2017-08-03 10:08:23 -0400

    Now inserting things into the database

--- database-playground.js
+++ database-playground.js
@@ -6,6 +6,24 @@ const database = pgPromise({ database: 'tiy-todos' })
 
 // ---- BOILERPLATE STOPS HERE ----
 
+
+// Insert someting into the Database
+database
+  // Ask the database to run a query that returns ONE thing (in this case the ID)
+  // The query is INSERT INTO ......
+  // The second argument is an array that fills in the placeholders $1 and $2
+  //    that avoids the "Little Bobby Tables" SQL Injection problem
+  //    $1 => 'Learn about SQL Injection' => description
+  //    $2 => false => completed
+  // ... and as the INSERT to *RETURN* the newly created `id`
+  .one('INSERT INTO "todos" (description, completed) VALUES($1, $2) RETURNING id',
+          ['Learn about SQL Injection', false])
+  // THEN, give me that new information as the variable `data`
+  .then(data => {
+    // Print out that data
+    console.log(`Our insert returned`, data)
+  })
+
 // Database, we are asking you for *ANY* number of rows
 // for the query in the string variable `query`
 // WHEN you have data, we are going to console log it
